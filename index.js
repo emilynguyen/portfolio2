40e2e6988b911b3df7c9b7477676f94b6a8e33b6
Emily Nguyen <emilymylinh@gmail.com>
2019-03-04 20:54:32 -0800

    Start adding projects

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ hbs.registerHelper("if_eq", function(a, b, opts) {
   if (a == b) return opts.fn(this);
   else return opts.inverse(this);
 });
+hbs.registerHelper("if_not_eq", function(a, b, opts) {
+  if (a != b) return opts.fn(this);
+  else return opts.inverse(this);
+});
 
 var app = express();
 
