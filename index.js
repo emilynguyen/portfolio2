b40bddf42bede8999b72299e0c79fcfff88e7d35
Emily Nguyen <emilymylinh@gmail.com>
2019-03-11 14:35:01 -0700

    New card design + accent color

--- index.js
+++ index.js
@@ -17,6 +17,62 @@ hbs.registerHelper("if_mod", function(a, b, opts) {
   else return opts.inverse(this);
 });
 
+hbs.registerHelper("get_darker", function(a, b, opts) {
+  var color1 = a;
+  var color2 = b;
+
+  function getLightness(hex) {
+    // If 3 digits, convert to 6
+    if (hex.length === 4) {
+      hex += hex.substring(1, 4);
+    }
+
+    var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+
+    var r = parseInt(result[1], 16);
+    var g = parseInt(result[2], 16);
+    var b = parseInt(result[3], 16);
+
+    (r /= 255), (g /= 255), (b /= 255);
+    var max = Math.max(r, g, b),
+      min = Math.min(r, g, b);
+    var h,
+      s,
+      l = (max + min) / 2;
+
+    if (max == min) {
+      h = s = 0; // achromatic
+    } else {
+      var d = max - min;
+      s = l > 0.5 ? d / (2 - max - min) : d / (max + min);
+      switch (max) {
+        case r:
+          h = (g - b) / d + (g < b ? 6 : 0);
+          break;
+        case g:
+          h = (b - r) / d + 2;
+          break;
+        case b:
+          h = (r - g) / d + 4;
+          break;
+      }
+      h /= 6;
+    }
+
+    s = s * 100;
+    s = Math.round(s);
+    l = l * 100;
+    l = Math.round(l);
+
+    var colorInHSL = "hsl(" + h + ", " + s + "%, " + l + "%)";
+
+    return l;
+  }
+
+  // Return color with lower lightness
+  return getLightness(color1) < getLightness(color2) ? color1 : color2;
+});
+
 var app = express();
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
